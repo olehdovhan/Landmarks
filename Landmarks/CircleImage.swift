3500963633029061a06b88d4da25be8e7ab5ec3b
olehDovhan <odovhan@3knbh.com>
2022-08-22 11:44:38 +0300

    DetailView

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by O l e h on 22.08.2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white,lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
