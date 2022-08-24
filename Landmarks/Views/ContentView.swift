f6ae98ec90a10ca4ac4bed56864e2a592f92fb36
olehDovhan <odovhan@3knbh.com>
2022-08-24 23:10:07 +0300

    fill List by data, set navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by O l e h on 22.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
